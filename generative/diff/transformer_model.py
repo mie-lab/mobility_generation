dd35b77bd3d1865833f14d45fe61d68123b9cca7
Ye <hongy_gz@foxmail.com>
2024-04-17 16:53:09 +0200

    small clean up

--- generative/diff/transformer_model.py
+++ generative/diff/transformer_model.py
@@ -1,6 +1,3 @@
-from transformers import AutoConfig
-
-# from transformers import BertEncoder
 from transformers.models.bert.modeling_bert import BertEncoder, BertConfig
 
 import torch
@@ -53,31 +50,11 @@ class TransformerNetModel(nn.Module):
 
         # embeds
         self.token_embedding = nn.Embedding(max_location, self.input_dims)
-        # self.dur_embedding = nn.Linear(1, self.input_dims)
-
-        # network for encoding
-        # self.embed_linear = nn.Linear(self.input_dims * 2, self.input_dims)
-        # self.embed_final = nn.Linear(self.input_dims, self.input_dims)
-        # self.embed_norm = nn.LayerNorm(self.input_dims)
-        # self.embed_norm2 = nn.LayerNorm(self.input_dims)
-        # self.linear1 = torch.nn.Linear(self.input_dims, self.input_dims * 4)
-        # self.linear2 = torch.nn.Linear(self.input_dims * 4, self.input_dims)
-        # self.dropout1 = nn.Dropout(p=0.1)
-        # self.dropout2 = nn.Dropout(p=0.1)
-
-        # network for decoding
-        # self.out_norm = nn.LayerNorm(self.input_dims)
-        # self.linear3 = torch.nn.Linear(self.input_dims, self.input_dims * 4)
-        # self.linear4 = torch.nn.Linear(self.input_dims * 4, self.input_dims)
-        # self.dropout3 = nn.Dropout(p=0.1)
-        # self.dropout4 = nn.Dropout(p=0.1)
 
         # heads
         self.lm_head = nn.Linear(self.input_dims, max_location)
-        # self.dur_head = nn.Linear(self.input_dims, 1)
         with torch.no_grad():
             self.lm_head.weight = self.token_embedding.weight
-            # self.dur_head.weight = nn.Parameter(self.dur_embedding.weight.T)
 
         self.time_embed = nn.Sequential(
             nn.Linear(hidden_t_dim, hidden_t_dim * 4),
@@ -85,26 +62,26 @@ class TransformerNetModel(nn.Module):
             nn.Linear(hidden_t_dim * 4, model_config.hidden_size),
         )
 
-        if self.input_dims != model_config.hidden_size:
+        if self.input_dims != self.hidden_size:
             self.input_up_proj = nn.Sequential(
-                nn.Linear(input_dims, model_config.hidden_size),
+                nn.Linear(input_dims, self.hidden_size),
                 nn.Tanh(),
-                nn.Linear(model_config.hidden_size, model_config.hidden_size),
+                nn.Linear(self.hidden_size, self.hidden_size),
             )
 
         self.input_transformers = BertEncoder(model_config)
 
         self.register_buffer("position_ids", torch.arange(model_config.max_position_embeddings).expand((1, -1)))
-        self.position_embeddings = nn.Embedding(model_config.max_position_embeddings, model_config.hidden_size)
-        self.LayerNorm = nn.LayerNorm(model_config.hidden_size, eps=model_config.layer_norm_eps)
+        self.position_embeddings = nn.Embedding(model_config.max_position_embeddings, self.hidden_size)
+        self.LayerNorm = nn.LayerNorm(self.hidden_size, eps=model_config.layer_norm_eps)
 
         self.dropout = nn.Dropout(model_config.hidden_dropout_prob)
 
-        if self.output_dims != model_config.hidden_size:
+        if self.output_dims != self.hidden_size:
             self.output_down_proj = nn.Sequential(
-                nn.Linear(model_config.hidden_size, model_config.hidden_size),
+                nn.Linear(self.hidden_size, self.hidden_size),
                 nn.Tanh(),
-                nn.Linear(model_config.hidden_size, self.output_dims),
+                nn.Linear(self.hidden_size, self.output_dims),
             )
 
         if learned_mean_embed:
@@ -121,25 +98,11 @@ class TransformerNetModel(nn.Module):
         x = self.linear4(self.dropout3(F.relu(self.linear3(x))))
         return self.dropout4(x)
 
-    def get_embeds(
-        self,
-        input_ids,
-        # input_durs,
-    ):
-        # out = torch.cat([self.token_embedding(input_ids), self.dur_embedding(input_durs.unsqueeze(-1))], dim=-1)
-        # out = self.embed_norm(self.embed_linear(out))
-
-        # out = self.embed_norm2(out + self._dense_block(out))
-        # return self.embed_final(out)
+    def get_embeds(self, input_ids):
         return self.token_embedding(input_ids)
 
     def get_logits(self, hidden_repr):
-        # out = self.out_norm(hidden_repr + self._out_block(hidden_repr))
-        # return self.lm_head(out)
         return self.lm_head(hidden_repr)
-
-    # def get_dur_predictions(self, hidden_repr):
-    #     return self.dur_head(hidden_repr)
 
     def forward(self, x, timesteps, padding_mask):
         """
@@ -158,7 +121,6 @@ class TransformerNetModel(nn.Module):
 
         seq_length = x.size(1)
         position_ids = self.position_ids[:, :seq_length]
-        # print(emb_x.shape, emb_t.shape, self.position_embeddings)
         emb_inputs = self.position_embeddings(position_ids) + emb_x + emb_t.unsqueeze(1).expand(-1, seq_length, -1)
         emb_inputs = self.dropout(self.LayerNorm(emb_inputs))
 
