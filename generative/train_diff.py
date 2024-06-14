efc34fbe487854a4801f0f8a27cc23be1d19327b
Ye <hongy_gz@foxmail.com>
2024-06-14 23:40:59 +0200

    lr schedule

--- generative/train_diff.py
+++ generative/train_diff.py
@@ -25,17 +25,14 @@ from generative.diff.step_sample import LossAwareSampler
 
 
 def _get_sqrt_schedule_with_warmup_lr_lambda(
-    current_step: int,
-    *,
-    num_warmup_steps: int,
-    min_decay: float,
-    num_training_steps: int,
-    num_cycles: float,
+    current_step: int, *, num_warmup_steps: int, min_decay: float, num_training_steps: int, num_cycles: int
 ):
     if current_step < num_warmup_steps:
         return float(current_step) / float(max(1, num_warmup_steps))
     progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-    return max(min_decay, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+    if progress >= 1.0:
+        return min_decay
+    return max(min_decay, 0.5 * (1.0 + math.cos(math.pi * ((float(num_cycles) * progress) % 1.0))))
 
 
 def get_sqrt_schedule_with_warmup(
